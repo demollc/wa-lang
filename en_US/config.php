ddf1eff68c4c01ea33846b53d747d1cc7f7b59f2
Alexkurd <alexkurd@gmail.com>
2019-03-13 23:25:39 +0300

    initial en_US

---
+++ en_US/config.php
@@ -0,0 +1,43 @@
+<?php
+return array (
+  'contacts' => 
+  array (
+    'version' => '1.1.6',
+  ),
+  'site' => 
+  array (
+    'version' => '2.5.5',
+  ),
+  'blog' => 
+  array (
+    'version' => '1.4.1',
+  ),
+  'photos' => 
+  array (
+    'version' => '1.2.8',
+  ),
+  'shop' => 
+  array (
+    'version' => '8.3.0',
+  ),
+  'installer' => 
+  array (
+    'version' => '1.10.10',
+  ),
+  'developer' => 
+  array (
+    'version' => '1.0.2',
+  ),
+  'helpdesk' => 
+  array (
+    'version' => '1.2.5',
+  ),
+  'mailer' => 
+  array (
+    'version' => '1.0.18',
+  ),
+  'crm' => 
+  array (
+    'version' => '1.4.0',
+  ),
+);
